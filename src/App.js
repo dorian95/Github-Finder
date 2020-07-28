52eddbc5ef0c9fb0d36785e9f842d8d537662660
Nursultan <dorian95@uw.edu>
2020-07-28 15:23:33 -0700

    initialized Navbar with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   // life cycle method, renders the output
   render() {
-    /*
-    // without JSX,
-    return React.createElement(
-      'div',
-      { className: 'App' },
-      React.createElement('h1', null, 'Hello from React')
-    );
-    */
-    const name = 'John Doe';
-    const loading = true;
-
     return (
       // JSX must have 1 parent element
       <div className='App'>
-        {loading ? <h4>Loading</h4> : <h1>Hello {name}</h1>}
+        <Navbar />
       </div>
     );
   }
