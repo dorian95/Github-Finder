54ab32322f1d92657ca934a8931e3bf9f9b50a87
Nursultan <dorian95@uw.edu>
2020-07-30 15:31:26 -0700

    used axios to send HTTP requests to get user profiles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  // life cycle method
+  async componentDidMount() {
+    // can't directly change state in class-based components in React
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+  }
+
   // life cycle method, renders the output
   render() {
     return (
@@ -11,7 +25,7 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
