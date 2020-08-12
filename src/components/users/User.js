1871a748133bdf460e070a8698f0f24c9247b2d2
Nursultan <dorian95@uw.edu>
2020-08-11 22:01:57 -0700

    User profile grid including info, such as Followers, following, etc

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -16,6 +16,7 @@ export class User extends Component {
   render() {
     const {
       name,
+      company,
       avatar_url,
       location,
       bio,
@@ -66,7 +67,36 @@ export class User extends Component {
             <a href={html_url} className='btn btn-dark my-1'>
               Visit Github Profile
             </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
           </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>PUblic Gists: {public_gists}</div>
         </div>
       </Fragment>
     );
