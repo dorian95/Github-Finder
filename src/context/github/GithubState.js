a8d5b17f58c2d60c3b4ece85f07647a3af311f16
Nursultan <dorian95@uw.edu>
2020-08-18 21:18:01 -0700

    moved Repos state to context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -54,7 +54,20 @@ const GithubState = (props) => {
       payload: res.data,
     });
   };
-  // Get Repos
+  // Get user's repos
+  const getUserRepos = async (username) => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+      client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data,
+    });
+  };
 
   // Clear users from state
   const clearUsers = () =>
@@ -76,6 +89,7 @@ const GithubState = (props) => {
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos,
       }}
     >
       {props.children}
