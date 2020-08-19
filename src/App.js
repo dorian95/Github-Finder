530be70f8cfecb89671c489b378fb8687d27d56d
Nursultan <dorian95@uw.edu>
2020-08-18 20:57:46 -0700

    App.js cleanup: removed searchUsers

--- src/App.js
+++ src/App.js
@@ -12,23 +12,10 @@ import './App.css';
 import About from './components/pages/About';
 
 const App = () => {
-  const [users, setUsers] = useState([]);
   const [user, setUser] = useState({});
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
-
-  // Search Github users
-  const searchUsers = async (text) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
-      client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    setUsers(res.data.items);
-    setLoading(false);
-  };
 
   // Get single Github user
   const getUser = async (username) => {
